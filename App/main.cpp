ed189b094bd2823663b0fa59a5da5ffada095010
Konstantin Zverev <konstantin.zverev@developex.org>
2016-05-19 17:30:25 +0300

    fix App build with OpenImageIO 1.7 + chmod for premake binary

--- App/main.cpp
+++ App/main.cpp
@@ -19,6 +19,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ********************************************************************/
+#include "OpenImageIO/imageio.h"
+
 #ifdef __APPLE__
 #include <OpenCL/OpenCL.h>
 #include <OpenGL/OpenGL.h>
@@ -34,7 +36,6 @@ THE SOFTWARE.
 #include <GL/glx.h>
 #endif
 
-#include "OpenImageIO/imageio.h"
 
 #include <memory>
 #include <chrono>
