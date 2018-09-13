439f95b6e1af7cb9491c18cc1c7b9526b44c91bc
Mikhail Kushnikov <mikhailkushnikov@Mikhails-MacBook-Pro-2.local>
2018-09-13 03:33:46 -0700

    kernel-modification-draft: PR cleanup

--- CLW/CMakeFiles/clw_kernel_cache_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clw_kernel_cache_h"
-  "clwkernels_cl.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clw_kernel_cache_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
